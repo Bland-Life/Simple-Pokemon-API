526e76a46befff0bd507dd4375c375bd96769389
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2023-07-10 19:25:34 -0400

    Create main.py
    Created the main python script and added in some very basic code to start the API

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Hello(Resource):
+    def get(self):
+        return "hello"
+
+api.add_resource(Hello, "/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
