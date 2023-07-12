aa625d9671d44aa4b9ecc6c2f78f15a299d062fb
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2023-07-12 14:40:34 -0400

    Update db_model.py
    Added dataclass decorator from the dataclasses library to help jsonify the data during query's of the database

--- db_model.py
+++ db_model.py
@@ -1,14 +1,16 @@
 from pokeapp import db
+from dataclasses import dataclass
 
+@dataclass
 class Pokemon(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        name = db.Column(db.String, unique=True, nullable=False)
-        species = db.Column(db.String)
-        types = db.Column(db.String)
-        height = db.Column(db.String)
-        weight = db.Column(db.String)
-        abilities = db.Column(db.String)
-        evolutions = db.Column(db.String)
-        facts = db.Column(db.String)
-        site_link = db.Column(db.String)
-        img_link = db.Column(db.String)
+        id: int = db.Column(db.Integer, primary_key=True)
+        name: str = db.Column(db.String, unique=True, nullable=False)
+        species: str = db.Column(db.String)
+        types: str = db.Column(db.String)
+        height: str = db.Column(db.String)
+        weight: str = db.Column(db.String)
+        abilities: str = db.Column(db.String)
+        evolutions: str = db.Column(db.String)
+        facts: str = db.Column(db.String)
+        site_link: str = db.Column(db.String)
+        img_link: str = db.Column(db.String)
