118bd2f7fa561788d1b46f4bef1b168d8730bbfb
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2023-07-12 14:41:46 -0400

    Update api_resources.py
    Added in a new resource to allow for query of ALL pokemon in the database

--- api_resources.py
+++ api_resources.py
@@ -1,5 +1,12 @@
+from flask import jsonify
 from flask_restful import Resource
+from pokeapp import db
+from db_model import Pokemon
 
 class Hello(Resource):
     def get(self):
         return "hello"
+    
+class Pokedex(Resource):
+    def get(self):
+        return jsonify(pokemon=Pokemon.query.all())
