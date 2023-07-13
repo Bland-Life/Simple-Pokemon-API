38a63a627f300d241c4a6486ae8821a4f6b7d9f0
Bland-Life <136954372+Bland-Life@users.noreply.github.com>
2023-07-13 15:50:51 -0400

    Update api_resources.py
    Refined the methods within the GET requests for each resource so that client's could get more accurate feedback when their requests return errors

--- api_resources.py
+++ api_resources.py
@@ -9,6 +9,13 @@ class Hello(Resource):
     
 class Pokedex(Resource):
     def get(self):
+        count = request.args.get("count")
+        if count and count.isdigit():
+            if int(count) > Pokemon.query.count():
+                return "Count is outside of current Pokedex range.", 400
+            return jsonify(pokemons=Pokemon.query.all()[:int(count)])
+        elif count and not count.isdigit():
+            return "Count must be a positive integer.", 400
         return jsonify(pokemons=Pokemon.query.all())
     
 class PokemonName(Resource):
@@ -17,9 +24,17 @@ class PokemonName(Resource):
         if poke_name:
             poke_name = poke_name.title()
             return jsonify(pokemon=Pokemon.query.filter_by(name=poke_name).first())
+        else:
+            return 'The "name" parameter is required and, currently, missing.', 400
         
 class PokemonID(Resource):
     def get(self, national_id):
         if not national_id:
-            return "No ID was given."
-        return jsonify(pokemon=Pokemon.query.get(national_id))
+            return "No ID was given.", 400
+
+        pokemon = Pokemon.query.get(national_id)
+        if pokemon:
+            return jsonify(pokemon=pokemon)
+        else:
+            return "Invalid Nationaldex ID" 
+        
